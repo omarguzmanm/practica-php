9100b5b83c1815842ed0c33c52fcc80d328dc3c7
Omar Guzmán <omaralexguzmanm21@gmail.com>
2023-08-31 19:48:45 -0600

    Se crearon Pruebas/Tests con PhpUnit y Guzzle

--- tests/PracticaTest.php
+++ tests/PracticaTest.php
@@ -7,14 +7,14 @@ final class PracticaTest extends TestCase
 {
     public function testFilesExistence(): void
     {
-        $this->assertFileExists('index.php');
+        $this->assertFileExists('usuarios.php');
         $this->assertFileExists('store.php');
         $this->assertFileExists('conexion.php');
     }
 
     public function testForm(): void
     {
-        $form = file_get_contents('index.php');
+        $form = file_get_contents('usuarios.php');
         $this->assertStringContainsStringIgnoringCase('action="store.php"', $form, $message = 'No se ha definido action');
         $this->assertStringContainsStringIgnoringCase('method="post"', $form, $message = 'No está asignado el método post');
     }
@@ -24,14 +24,14 @@ final class PracticaTest extends TestCase
         $form = file_get_contents('store.php');
         $this->assertStringContainsStringIgnoringCase('$_POST', $form, $message = 'No se utiliza $_POST');
         $this->assertStringContainsStringIgnoringCase('header(', $form, $message = 'No se encuenra método header()');
-        $this->assertStringContainsStringIgnoringCase('Location: index.php', $form, $message = 'No se redirecciona hacia index.php');
+        $this->assertStringContainsStringIgnoringCase('refresh:3;url=usuarios.php', $form, $message = 'No se redirecciona hacia usuarios.php');
         $this->assertStringContainsStringIgnoringCase('INSERT INTO', $form, $message = 'No se llama a INSERT INTO');
     }
 
     public function testPost(): void
     {
         $client = new Client();
-        $response = $client->post('http://localhost/practica-php/store.php', [
+        $response = $client->post('http://localhost/practica-php/usuarios.php', [
                 'form_params' => [
                     'nombre' => 'Prueba Nombre',
                     'correo' => 'prueba@test.com',
